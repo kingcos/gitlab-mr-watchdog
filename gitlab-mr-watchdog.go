f086dfc7d6504b9460dba6b5523913e8c58948e1
kingcos <2821836721v@gmail.com>
2018-11-06 16:15:14 +0800

    Add fetchGroupProjects func

--- gitlab-mr-watchdog.go
+++ gitlab-mr-watchdog.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	yaml "gopkg.in/yaml.v2"
@@ -17,6 +19,7 @@ type TriggerConfig struct {
 		Group    string `yaml:"group"`
 		Username string `yaml:"username"`
 		Project  string `yaml:"project"`
+		Token    string `yaml:"token"`
 	} `yaml:"GitLab"`
 }
 
@@ -41,6 +44,8 @@ func (config *TriggerConfig) validate() {
 		err = errors.New("GitLab group or username is required")
 	case config.GitLab.Project == "":
 		err = errors.New("GitLab project is required")
+	case config.GitLab.Token == "":
+		err = errors.New("GitLab token is required")
 	}
 
 	if err != nil {
@@ -60,17 +65,76 @@ func printErrorThenExit(err error, message string) {
 	}
 }
 
+// GitLabUtility struct for GitLab uitility properties & funcs
+type GitLabUtility struct {
+	host     string
+	token    string
+	username string
+	group    string
+}
+
+// GitLabGroupResponse for GitLab API response of `group`
+type GitLabGroupResponse struct {
+	Projects []GitLabProject `json:"projects"`
+}
+
+// GitLabProject for project structure in GitLabGroupResponse
+type GitLabProject struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Fetch GitLab group's projects
+func (utility *GitLabUtility) fetchGroupProjects() ([]GitLabProject, error) {
+	apiURL := utility.host + "/api/v4/groups/" + utility.group
+	request, _ := http.NewRequest("GET", apiURL, nil)
+	request.Header.Set("Private-Token", utility.token)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return []GitLabProject{}, err
+	}
+
+	defer response.Body.Close()
+
+	// Print info when success or failure
+	switch response.StatusCode {
+	case 200:
+		body, _ := ioutil.ReadAll(response.Body)
+		var model GitLabGroupResponse
+
+		json.Unmarshal(body, &model)
+
+		return model.Projects, errors.New("----")
+	case 404:
+		return []GitLabProject{}, errors.New("----")
+	default:
+		body, _ := ioutil.ReadAll(response.Body)
+		return []GitLabProject{}, errors.New("Unknown: " + string(body))
+	}
+}
+
+// Fetch GitLab group info
+
 func main() {
 	// Read config file path from command line
-	var configFilePath = flag.String("path", "config.yml", "Path (default config.yml)")
+	var configFilePath = flag.String("path", "config.yml", "Setup your configuration file path.")
 	flag.Parse()
-	if *configFilePath == "" {
-		printErrorThenExit(errors.New("path is required"), "Nil argument error")
-	}
 
 	// Read & validate config.yml
 	var config TriggerConfig
 	config.read(*configFilePath)
 	config.validate()
 
+	gitlab := GitLabUtility{}
+	gitlab.host = config.GitLab.Host
+	gitlab.group = config.GitLab.Group
+	gitlab.username = config.GitLab.Username
+	gitlab.token = config.GitLab.Token
+
+	// projects, err := gitlab.fetchGroupProjects()
+	// printErrorThenExit(err, "")
+
+	fmt.Println(gitlab)
 }
