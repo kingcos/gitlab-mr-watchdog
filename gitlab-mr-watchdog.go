6ac24a3bd62ec8859450705f63b8ecc913033a32
kingcos <2821836721v@gmail.com>
2018-11-06 21:12:29 +0800

    Do sth during some durations

--- gitlab-mr-watchdog.go
+++ gitlab-mr-watchdog.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -21,6 +22,9 @@ type WatchdogConfig struct {
 		Project  string `yaml:"project"`
 		Token    string `yaml:"token"`
 	} `yaml:"GitLab"`
+	Watchdog struct {
+		Duration float64 `yaml:"duration"`
+	} `yaml:"Watchdog"`
 }
 
 // Read config YAML file, then return Config
@@ -84,6 +88,48 @@ type GitLabProject struct {
 	Name string `json:"name"`
 }
 
+// GitLabMergeRequest for GitLab merge requests structure
+type GitLabMergeRequest struct {
+	IID       string `json:"iid"`
+	Title     string `json:"title"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	WIP       string `json:"work_in_progress"`
+	Author    struct {
+		Name     string `json:"name"`
+		Username string `json:"username"`
+	} `json:"author"`
+}
+
+// Fetch GitLab merge requests by ID
+func (utility *GitLabUtility) fetchMergeRequestsByID(id int, params string) ([]GitLabMergeRequest, error) {
+	apiURL := utility.host + "/api/v4/projects/" + string(id) + "/merge_requests/" + params
+	request, _ := http.NewRequest("GET", apiURL, nil)
+	request.Header.Set("Private-Token", utility.token)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return []GitLabMergeRequest{}, err
+	}
+
+	defer response.Body.Close()
+
+	// Print info when success or failure
+	switch response.StatusCode {
+	case 200:
+		body, _ := ioutil.ReadAll(response.Body)
+		var models []GitLabMergeRequest
+
+		json.Unmarshal(body, &models)
+
+		return models, nil
+	default:
+		body, _ := ioutil.ReadAll(response.Body)
+		return []GitLabMergeRequest{}, errors.New("Unknown: " + string(body))
+	}
+}
+
 // Fetch GitLab group's projects
 func (utility *GitLabUtility) fetchGroupProjects() ([]GitLabProject, error) {
 	apiURL := utility.host + "/api/v4/groups/" + utility.group
@@ -115,8 +161,6 @@ func (utility *GitLabUtility) fetchGroupProjects() ([]GitLabProject, error) {
 	}
 }
 
-// Fetch GitLab group info
-
 func main() {
 	// Read config file path from command line
 	var configFilePath = flag.String("path", "config.yml", "Setup your configuration file path.")
@@ -136,5 +180,23 @@ func main() {
 	projects, err := gitlab.fetchGroupProjects()
 	printErrorThenExit(err, "")
 
-	fmt.Println(len(projects))
+	projectID := -1
+	// Get GitLab group's project by ID
+	for _, project := range projects {
+		if project.Name == config.GitLab.Project {
+			projectID = project.ID
+		}
+	}
+
+	if projectID == -1 {
+		printErrorThenExit(errors.New(""), "")
+	}
+
+	gitlab.fetchMergeRequestsByID(projectID, "?state=opened")
+
+	for {
+		time.AfterFunc(time.Duration(config.Watchdog.Duration)*time.Minute, func() {
+			// do sth...
+		})
+	}
 }
